7c660cbe5f6fc2bffb78ec8c887aa10a8ae54579
CRImier <crimier@yandex.ru>
2025-02-15 19:37:49 +0100

    i forgor

--- test_code/main.py
+++ test_code/main.py
@@ -1,7 +1,9 @@
 from machine import Pin, I2C, PWM
 from time import sleep
 
-i2c = I2C(1, scl=Pin(27), sda=Pin(26), freq=100_000)
+#import usb_hid
+
+i2c = I2C(1, scl=Pin(27), sda=Pin(26), freq=400_000)
 print(i2c.scan())
 
 bl = PWM(Pin(29))
@@ -44,37 +46,119 @@ for i in range(3):
         shiftOut(i)
         sleep(0.5)
 
-r = [12, 0, 13, 1, 14, 2, 15, 3]
-c = [16, 4, 17, 5, 18, 6, 19, 7, 20, 8, 21, 9, 22, 10, 23, 11]
+def test_shorts():
+    r = [12, 0, 13, 1, 14, 2, 15, 3]
+    c = [16, 4, 17, 5, 18, 6, 19, 7, 20, 8, 21, 9, 22, 10, 23, 11]
+    o = [24, 25, 26, 27, 28]
+    p = r+c+o
+    pins = [Pin(x, Pin.IN, Pin.PULL_UP) for x in p]
 
-rows = []
-cols = []
+    r_n = ["KSI"+str(i) for i in range(len(r))]
+    c_n = ["KSO"+str(i) for i in range(len(c))]
+    o_n = ["TP_ID", "SW", "TP_SDA", "TP_SCL", "TP_INT"]
+    p_n = r_n + c_n + o_n
 
-for row in r:
-    p = Pin(row, Pin.OUT)
-    p.on()
-    rows.append(p)
-
-for col in c:
-    p = Pin(col, Pin.IN, Pin.PULL_UP)
-    cols.append(p)
-
-pressed = []
-
-def test():
-    for ri, row in enumerate(rows):
-        row.off()
-        for ci, col in enumerate(cols):
-            st = col.value()
-            if not st:
-                name = "{}-{}".format(ri, ci)
-                if name in pressed:
-                    print("Already pressed", name, "!")
+    while True:
+        for _ in range(1000000):
+            shorted = []
+            for i, pin1 in enumerate(pins):
+                pin1.init(Pin.OUT)
+                pin1.value(False)
+                for j, pin2 in enumerate(pins):
+                    if i == j:
+                        continue
+                    if not pin2.value():
+                        shorted.append((i, j))
+                pin1.init(Pin.IN, Pin.PULL_UP)
+            if shorted:
+                # see if everything is shorted to everything hehe
+                print(_, ",")
+                if len(shorted) < len(p)*(len(p)-1) // 2:
+                    # just a few shorted, gotta list those
+                    for i, j in shorted:
+                        print("n: {} ({}) shorted to {} ({})".format(p_n[i], p[i], p_n[j], p[j]))
                 else:
-                    print("Pressed:", name)
-                    pressed.append(name)
-        row.on()
-        #sleep(0.01)
-    sleep(0.2)
+                    # most of them shorted, we list exceptions instead
+                    l = []
+                    for i, j in shorted: # unsophisticated flattening
+                        if i not in l: l.append(i)
+                        if j not in l: l.append(j)
+                    for i, pin in enumerate(p):
+                        if i not in l:
+                            print("m: {} ({}) not connected".format(p_n[i], pin))
+                    #print(shorted)
+            sleep(0.05)
 
-while True: test()
+def test_keeb():
+    r = [12, 0, 13, 1, 14, 2, 15, 3]
+    c = [16, 4, 17, 5, 18, 6, 19, 7, 20, 8, 21, 9, 22, 10, 23, 11]
+    r_t = [False for i in r]
+    c_t = [False for i in c]
+    r_n = ["KSI"+str(i) for i in range(len(r))]
+    c_n = ["KSO"+str(i) for i in range(len(c))]
+
+    rows = []
+    cols = []
+
+    for row in r:
+        p = Pin(row, Pin.OUT)
+        p.on()
+        rows.append(p)
+
+    for col in c:
+        p = Pin(col, Pin.IN, Pin.PULL_UP)
+        cols.append(p)
+
+    pressed = []
+    try:
+        while True:
+            for ri, row in enumerate(rows):
+                row.off()
+                for ci, col in enumerate(cols):
+                    st = col.value()
+                    if not st:
+                        name = "{}-{}".format(ri, ci)
+                        r_t[ri] = True
+                        c_t[ci] = True
+                        if name in pressed:
+                            pass # print("Already pressed", name, "!")
+                        else:
+                            print("Pressed:", name)
+                            pressed.append(name)
+                row.on()
+                #sleep(0.01)
+            sleep(0.2)
+    except KeyboardInterrupt:
+        unc_r = [r_n[i] for i, el in enumerate(r_t) if not el]
+        unc_c = [c_n[i] for i, el in enumerate(c_t) if not el]
+        print("Faulty: ", end='')
+        if unc_r+unc_c:
+            print(",".join(unc_r+unc_c))
+        else:
+            print("none")
+
+
+
+def test_i2c():
+    while True:
+      try:
+        l = i2c.readfrom(0x2c, 1)[0]
+        if l:
+            d = i2c.readfrom(0x2c, l)
+            if d[2] != 0x01:
+                # only forward packets with a specific report ID, discard all others
+                print("WARNING")
+                print(l, d)
+                print("WARNING")
+            else:
+                d = d[3:]
+                print(l, len(d), d)
+                #usb_hid.report(usb_hid.MOUSE_ABS, d)
+      except OSError:
+        # touchpad unplugged? retry in a bit
+        sleep(0.01)
+
+#print("short testing!")
+#test_shorts()
+#test_keeb()
+test_i2c()
